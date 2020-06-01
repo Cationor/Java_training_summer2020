7e39a93be7b48484d3180e0ede462d32dd537091
kiryl_kashuba <kashuba.kirya@gmail.ru>
2020-06-01 16:50:28 +0300

    Finished task 10

--- src/com/epamcourse/homework1/task1/Main.java
+++ src/com/epamcourse/homework1/task1/Main.java
@@ -1,4 +1,9 @@
 package com.epamcourse.homework1.task1;
+import com.epamcourse.homework1.task10.SegmentFunction;
+import com.epamcourse.homework1.task2.LeapYear;
+import com.epamcourse.homework1.task3.SquareAndCircle;
+import com.epamcourse.homework1.task4.EvenNumbers;
+import com.epamcourse.homework1.task5.PerfectNumber;
 import com.epamcourse.homework1.task6.Seconds;
 import com.epamcourse.homework1.task7.TwoPoints;
 import com.epamcourse.homework1.task8.Function;
@@ -9,31 +14,34 @@ public class Main {
 
 //        LastNumber lastNumber = new LastNumber();  // Task 1
 //        lastNumber.run();
-
+//
 //        LeapYear leapYear = new LeapYear(); // Task 2
 //        leapYear.run();
-
-
+//
+//
 //        SquareAndCircle squareAndCircle = new SquareAndCircle(); //Task 3
 //        squareAndCircle.run();
-
+//
 //        EvenNumbers evenNumbers = new EvenNumbers(); // Task 4
 //        evenNumbers.run();
-
+//
 //        PerfectNumber perfectNumber = new PerfectNumber(); // Task 5
 //        perfectNumber.run();
-
-//        Seconds second = new Seconds();
+//
+//        Seconds second = new Seconds(); // Task 6
 //        second.run();
-
-//        TwoPoints twoPoints = new TwoPoints();
+//
+//        TwoPoints twoPoints = new TwoPoints(); // Task 7
 //        twoPoints.run();
-
-//        Function function = new Function();
+//
+//        Function function = new Function(); // Task 8
 //        function.run();
+//
+//        LengthAndAreaOfCircle lengthAndAreaOfCircle = new LengthAndAreaOfCircle(); // Task 9
+//        lengthAndAreaOfCircle.run();
 
-        LengthAndAreaOfCircle lengthAndAreaOfCircle = new LengthAndAreaOfCircle();
-        lengthAndAreaOfCircle.run();
+        SegmentFunction segmentFunction = new SegmentFunction();
+        segmentFunction.run();
 
     }
 }
